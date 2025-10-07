1fd554fe988e89d432ec6a1f507270eb506f8f94
Yorick Nap <144012023+codeyorick@users.noreply.github.com>
2025-10-07 08:22:01 +0200

    Refactor role-based access control by merging admin layout logic into global layout and updating route protection

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,18 @@
 import type { LayoutServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
+import { requireRole } from '$lib/server/utils/auth';
 
 export const load: LayoutServerLoad = async ({ cookies, locals: { sessionPromise }, url }) => {
-	if ((await sessionPromise)?.user) {
+	const user = (await sessionPromise)?.user;
+
+	if (user) {
 		if (url.pathname === '/auth/login') redirect(303, '/');
 	} else {
 		if (!url.pathname.startsWith('/auth')) redirect(303, '/auth/login');
+	}
+
+	if (url.pathname.startsWith('/admin')) {
+		await requireRole(user, 'admin');
 	}
 
 	return {
--- src/routes/admin/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-import { requireRole } from '$lib/server/utils/auth';
-
-export const load: LayoutServerLoad = async ({ locals: { sessionPromise } }) => {
-	await requireRole((await sessionPromise)?.user, 'admin');
-	return {};
-};
