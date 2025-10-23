423a2efd62c2a2b1a64799c7cfd2167b1de7f9b3
Yorick Nap <144012023+codeyorick@users.noreply.github.com>
2025-10-23 19:32:31 +0200

    Refactor data fetching in +page.svelte to use await blocks and handle errors

--- src/routes/admin/settings/instruments/+page.svelte
+++ src/routes/admin/settings/instruments/+page.svelte
@@ -2,28 +2,32 @@
 	import DataTable from './data-table.svelte';
 	import { Skeleton } from '$lib/components/ui/skeleton';
 	import { getInstruments } from '$lib/remote/admin/scores.remote';
+	import { Issues } from '$lib/components/issues';
 </script>
 
 <h1 class="text-2xl font-bold mb-4">Instrumenten</h1>
-<svelte:boundary>
-	{#snippet pending()}
-		<div class="grid grid-cols-3 gap-4 mt-12">
-			<Skeleton class="h-8 col-span-3" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-		</div>
-	{/snippet}
 
-	<DataTable data={await getInstruments()} />
+{#await getInstruments()}
+	<div class="grid grid-cols-3 gap-4 mt-12">
+		<Skeleton class="h-8 col-span-3" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+	</div>
+{:then data}
 
-</svelte:boundary>
+	<DataTable {data} />
+
+{:catch _error}
+	<Issues
+		issues={[{message: 'Er is een fout opgetreden bij het laden van de instrumenten. Probeer het later opnieuw.'}]} />
+{/await}
--- src/routes/admin/settings/users/+page.svelte
+++ src/routes/admin/settings/users/+page.svelte
@@ -2,28 +2,32 @@
 	import DataTable from './data-table.svelte';
 	import { Skeleton } from '$lib/components/ui/skeleton';
 	import { getUsers } from '$lib/remote/admin/users.remote';
+	import { Issues } from '$lib/components/issues';
 </script>
 
 <h1 class="text-2xl font-bold mb-4">Gebruikers</h1>
-<svelte:boundary>
-	{#snippet pending()}
-		<div class="grid grid-cols-3 gap-4 mt-12">
-			<Skeleton class="h-8 col-span-3" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-			<Skeleton class="h-8" />
-		</div>
-	{/snippet}
 
-	<DataTable data={await getUsers()} />
+{#await getUsers()}
+	<div class="grid grid-cols-3 gap-4 mt-12">
+		<Skeleton class="h-8 col-span-3" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+		<Skeleton class="h-8" />
+	</div>
+{:then data}
 
-</svelte:boundary>
+	<DataTable {data} />
+
+{:catch _error}
+	<Issues
+		issues={[{message: 'Er is een fout opgetreden bij het laden van de gebruikers. Probeer het later opnieuw.'}]} />
+{/await}
