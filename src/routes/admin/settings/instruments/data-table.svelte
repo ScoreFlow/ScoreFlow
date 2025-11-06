464990b812e1417cfb992c8797c57e84656214ea
Yorick Nap <144012023+codeyorick@users.noreply.github.com>
2025-11-06 17:31:32 +0100

    Add fallback message to empty data tables in instruments and users settings (#141)

--- src/routes/admin/settings/instruments/data-table.svelte
+++ src/routes/admin/settings/instruments/data-table.svelte
@@ -81,6 +81,12 @@
 							</TableCell>
 						{/each}
 					</TableRow>
+				{:else}
+					<TableRow>
+						<TableCell colspan={columns.length} class="h-24 text-center">
+							Geen resultaten.
+						</TableCell>
+					</TableRow>
 				{/each}
 			</TableBody>
 		</Table>
